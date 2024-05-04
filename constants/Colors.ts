bedf82d9ab92ae2073809328faeff22c1e4c435d
ZhenyaKelarev <zeka416351@gmail.com>
2024-05-04 13:28:54 +0300

    add tabs for expo project

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,22 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc"
+const tintColorDark = "#fff"
 
 export default {
+  primary: "#FF385C",
+  grey: "#5E5D5E",
+  black: "#1A1A1A",
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
-};
+}
